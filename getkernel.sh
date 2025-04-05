c40ddca99b29bff6cde1779f522de1a2ee6bb343
Mikku Chann <mikua4445@gmail.com>
2025-04-05 18:10:51 +0000

    modified for one click archive & upload (for zephyrus kernel)

---
+++ getkernel.sh
@@ -0,0 +1,1 @@
+rm -rf zephyruskernel.zip && rm -rf Image.gz-dtb && rm -rf dtbo.img && cp ../zephyrus-kernel-r5x/out/arch/arm64/boot/Image.gz-dtb ./ && cp ../zephyrus-kernel-r5x/out/arch/arm64/boot/dtbo.img ./ && zip -r9 zephyruskernel * && curl uploader.sh -T zephyruskernel.zip
