398ff17a89238ae1a6914e8a8ff200ca67f5e294
MikuChan <116325885+Mikku-chan@users.noreply.github.com>
2025-06-05 13:41:10 +0700

    Update getkernel.sh
    make it specific for zephyrus kernel.

--- getkernel.sh
+++ getkernel.sh
@@ -1,1 +1,1 @@
-rm -rf yourkernel.zip && rm -rf Image.gz-dtb && rm -rf dtbo.img && cp ../your-kernel-folder/out/arch/arm64/boot/Image.gz-dtb ./ && cp ../your-kernel-folder/out/arch/arm64/boot/dtbo.img ./ && zip -r9 yourkernelname * && curl uploader.sh -T yourkernelname.zip
+rm -rf zephyrus.zip && rm -rf Image.gz-dtb && rm -rf dtbo.img && cp ../zephyrus-kernel-r5x/out/arch/arm64/boot/Image.gz-dtb ./ && cp ../zephyrus-kernel-r5x/out/arch/arm64/boot/dtbo.img ./ && zip -r9 zephyrus * && curl uploader.sh -T zephyrus.zip
